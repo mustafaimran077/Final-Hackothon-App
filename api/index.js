a4cfc3f99a82ab7db2ceafaf00a82f763362b9ae
mustafaimran077 <mustafaimran077@gmail.com>
2025-11-19 22:01:49 +0500

    uPDATED

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import app from "../src/server.js";
 import connectDB from "../src/lib/lib.js";
 
-export default async function handler(req, res) {
-  await connectDB();     // Safe for Serverless
-  return app(req, res);  // Pass request to Express
-}
+// Establish the database connection when the function starts up.
+// 'connectDB' is smart enough to use the cached connection after the first run.
+connectDB();
+
+// Export the Express app directly. Vercel automatically wraps it.
+export default app;
--- src/server.js
+++ src/server.js
@@ -2,21 +2,30 @@ import express from "express";
 import cors from "cors";
 
 import authRoutes from "./Routes/authRoute.js";
-import bookRoutes from "./Routes/bookRoute.js";
+
 import appoinmentRoute from "./Routes/appoinmentRoute.js";
 import medicalRecordRoutes from "./Routes/medicallRecordRoute.js";
 import doctorRoutes from "./Routes/doctorRoutes.js";
 import profileRoute from "./Routes/profileRoute.js";
+import { connect } from "mongoose";
+import connectDB from "./lib/lib.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/books", bookRoutes);
+
 app.use("/api/appoinment", appoinmentRoute);
 app.use("/api/records", medicalRecordRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/users", profileRoute);
+
+// connectDB()
+
+// app.listen(process.env.PORT,()=>{
+//     console.log("OKAY");
+    
+// })
 
 export default app;
