d7147ac48a3024ee4ab7cde922ca16bd5a9ae697
mustafaimran077 <mustafaimran077@gmail.com>
2025-11-19 23:21:10 +0500

    C

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ import express from "express";
 import cors from "cors";
 
 import authRoutes from "./Routes/authRoute.js";
-
 import appoinmentRoute from "./Routes/appoinmentRoute.js";
 import medicalRecordRoutes from "./Routes/medicallRecordRoute.js";
 import doctorRoutes from "./Routes/doctorRoutes.js";
 import profileRoute from "./Routes/profileRoute.js";
-import { connect } from "mongoose";
+
 import connectDB from "./lib/lib.js";
 
 const app = express();
@@ -15,23 +14,21 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/appoinment", appoinmentRoute);
 app.use("/api/records", medicalRecordRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/users", profileRoute);
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "server start",
-  });
+  res.json({ message: "Server started" });
 });
 
-connectDB()
+const PORT = process.env.PORT || 8000;
 
-app.listen(process.env.PORT,()=>{
-    console.log("OKAY");
-    
-})
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("Failed to connect to DB:", err.message));
 
 export default app;
