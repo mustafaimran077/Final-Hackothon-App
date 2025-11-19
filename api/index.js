7f5725c13c17e4a0cbc454b99283a4c1f848ef30
mustafaimran077 <mustafaimran077@gmail.com>
2025-11-19 22:57:18 +0500

    Corect

--- api/index.js
+++
@@ -1,9 +0,0 @@
-// import app from "../src/server.js";
-// import connectDB from "../src/lib/lib.js";
-
-// // Establish the database connection when the function starts up.
-// // 'connectDB' is smart enough to use the cached connection after the first run.
-// connectDB();
-
-// // Export the Express app directly. Vercel automatically wraps it.
-// export default app;
